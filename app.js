0d0aeaa9cd2d7cd656f203135641764bbe11431b
sonuahluwalia <sonuahluwalia@hotmail.com>
2021-06-05 22:43:14 +0530

    Added pring and creation js to modularize the code futher

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// This is the main file acting as controller with the main function and
-// validation and matrix creation logic
+// This is the main file acting as controller with the main function 
 
 const readline = require("readline-sync");
 var clc = require("cli-color");
@@ -8,6 +7,8 @@ var subtraction = require('./lib/subtraction.js');
 var diagonal = require('./lib/diagonal.js');
 var transpose = require('./lib/transpose.js');
 var print = require('./lib/print.js');
+var validate = require('./lib/validation.js');
+var create = require('./lib/creation.js');
 
 
 var input;
@@ -31,6 +32,7 @@ main();
 function main() {
     do {
         array1 = [], array2 = [], valid = false;
+        console.clear();
         menu();
         input = readline.questionInt(clc.yellowBright("Enter Your Option: "));
 
@@ -39,30 +41,30 @@ function main() {
                 console.log(clc.yellowBright("Program Exited."));
                 return;
             case 1:
-                createTwoMatrix();
+                create.createTwoMatrix(array1, array2);
                 console.log("This is case 1");
                 // console.log(array1.length);
                 // console.log(array2.length);
-                if (validateTwoArray(array1, array2)) {
+                if (validate.validateTwoArray(array1, array2)) {
                     // var sum = addition(array1, array2, rows, columns);
-                    print.print2DArray(addition(array1, array2, rows, columns));
+                    print.print2DArray(addition(array1, array2));
                 } else {
                     console.log(clc.yellowBright("Not a square matrix"));
                 }
                 break;
             case 2:
-                createTwoMatrix();
+                create.createTwoMatrix(array1, array2);
                 // console.log("This is case 2");
-                if (validateTwoArray(array1, array2)) {
-                    print.print2DArray(subtraction(array1, array2, rows, columns));
+                if (validate.validateTwoArray(array1, array2)) {
+                    print.print2DArray(subtraction(array1, array2));
                 } else {
                     console.log(clc.yellowBright("Not a square matrix"));
                 }
                 break;
             case 3:
-                createOneMatrix();
+                create.createOneMatrix(array1);
                 // console.log("This is case 3");
-                if (validateOneArray(array1)) {
+                if (validate.validateOneArray(array1)) {
                     var obj = diagonal(array1);
                     console.log(clc.blueBright("Major Diagonal"));
                     print.print1DArray(obj.MajorDiagonal);
@@ -74,9 +76,9 @@ function main() {
                 }
                 break;
             case 4:
-                createOneMatrix();
+                create.createOneMatrix(array1);
                 // console.log("This is case 4");
-                if (validateOneArray(array1)) {
+                if (validate.validateOneArray(array1)) {
                     print.print2DArray(transpose(array1, array1.length));
                 } else {
                     console.log(clc.yellowBright("Not a square matrix"));
@@ -87,101 +89,11 @@ function main() {
                 break;
         }
         take = readline.question(clc.yellowBright("Do you want to continue(y/n) : "));
-        valid = validInputYorN(take);
-        console.clear();
+        valid = validate.validInputYorN(take);
+
     } while (valid)
 
 }
 
-// array creation and fill array
-function createTwoMatrix() {
-    array1 = createMatrix(array1, 1);
-    print.print2DArray(array1);
-    array2 = createMatrix(array2, 2);
-    print.print2DArray(array2);
-}
-
-function createOneMatrix() {
-    array1 = createMatrix(array1, 1);
-    print.print2DArray(array1);
-}
-
-
-var rows = 0, columns = 0;
-function createMatrix(array, num) {
-    rows = readline.questionInt(clc.blueBright(
-        "How many rows do you want for your " + num + " matrix to have? "
-    ));
-    columns = readline.questionInt(clc.blueBright(
-        "How many columns do you want for your  " + num + " matrix to have? "
-    ));
-
-    for (var i = 0; i < rows; i++) {
-        array.push([]);
-    }
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < columns; j++) {
-            array[i][j] = readline.questionInt(clc.greenBright(`Value in ${i} and ${j} = `));
-        }
-    }
-    // console.log(array);
-    return array;
-}
-
-// Validation code below
-
-function validInputYorN(take) {
-    var input = "";
-    if (useRegex(take)) {
-        if (take === 'Y' || take === 'y') {
-            console.log(take);
-            return true;
-        } else if (take === 'N' || take === 'n') {
-            console.log(take)
-            return false;
-        }
-    } else {
-        input = otherInput(take);
-        validInputYorN(input);
-    }
-}
-function otherInput(take) {
-    console.log(take)
-    console.log(clc.yellowBright("Invalid Option. Please choose (y/n)"));
-    input = readline.question(clc.yellowBright("Do you want to continue(y/n) : "));
-    return input;
-}
-// console.log(useRegex("y"))
-function useRegex(input) {
-    // let regex = /^Y | y | N | n$/i;
-    let regex = /^[Y|y|N|n]?$/i;
-    // let blankregex = /^$/;
-    // if (input.length === 0)
-    //     return !blankregex.test(input);
-    // else
-    return regex.test(input);
-}
-function validateTwoArray(array1, array2) {
-    // console.log(array1.length);
-    // console.log(array2.length);
-    // console.log("I am in validate two array");
-    var isLengthEqual = false;
-    if (array1.length == array2.length) {
-        isLengthEqual = true;
-    }
-    return isLengthEqual;
-
-}
-
-function validateOneArray(array1) {
-
-    var numberOfRows = array1.length // row
-    var numberOfColumns = array1[0].length // col
-    if (numberOfRows === numberOfColumns) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 
